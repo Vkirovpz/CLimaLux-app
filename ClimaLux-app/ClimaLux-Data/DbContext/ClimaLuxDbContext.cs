b225485243fe621bb16480186619fe546b6ad58c
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-06-23 22:14:09 +0300

    Create DbContext and Db sets

---
+++ ClimaLux-app/ClimaLux-Data/DbContext/ClimaLuxDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ClimaLux_Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimaLux_Data.DbContext
+{
+    public class ClimaLuxDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ClimaLuxDbContext(DbContextOptions<ClimaLuxDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Climatic> Climatics { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<BtuPower> BtuPowers { get; set; }
+        public DbSet<RoomSize> RoomSizes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<EnergyClassCooling> EnergyClassCoolings { get; set; }
+        public DbSet<EnergyClassHeating> EnergyClassHeatings { get; set; }
+    }
+}
